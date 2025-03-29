f52c288c1801e451ed168ae9b7113e956f7f0377
fooiskandar <fooiskandar@gmail.com>
2025-03-29 21:48:29 +0700

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-micro.vercel.app",
+  site: "https://fooiskandar.web.id",
   integrations: [sitemap(), mdx(), pagefind()],
   vite: {
     plugins: [tailwindcss()],
